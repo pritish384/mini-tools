8268e914bdae07a2fc70ced623ff9708eac41aeb
Pritish Purav <90410463+pritish384@users.noreply.github.com>
2023-08-19 11:58:52 +0530

    heartbeat bug

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,15 @@
 from waitress import serve
 from app import app  # Import your Flask app instance
+import time
+import threading
+
+def alive():
+    while True:
+        print("Heartbeat 💖")
+        time.sleep(5)
+
+
+threading.Thread(target=alive).start()
 
 if __name__ == "__main__":
     serve(app, host='0.0.0.0', port=80)
