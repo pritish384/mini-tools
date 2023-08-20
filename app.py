3271e041dbd2a56b8db3150dd9c5717903c00700
Pritish Purav <90410463+pritish384@users.noreply.github.com>
2023-08-20 23:16:04 +0530

    error bug fix

--- app.py
+++ app.py
@@ -40,8 +40,9 @@ def yt_video_downloader():
         download_url = f"https://www.youtube.com/watch?v={query}"
         try:
             download_url = get_video_download_url(download_url)
-        except:
-            return render_template('templates/error.html' , error="Error while downloading the video")
+        except Exception as e:
+
+            return render_template('templates/error.html' , error=f"{e}" , solution="Please ensure that the video is not set to private, is not in a live streaming state, and is available in your country.")
         return redirect(download_url)
 
 
@@ -59,7 +60,7 @@ def yt_video_downloader_query():
     
 
     yt_api_key = os.getenv('YOUTUBE_API_KEY')
-    yt_api = build('youtube' , 'v3' , developerKey=yt_api_key ,static_discovery=False)
+    yt_api = build('youtube' , 'v3' , developerKey=yt_api_key)
     search_response = yt_api.search().list(
         part='snippet',
         q=query,
@@ -88,6 +89,27 @@ def stock_market_range_query():
     
     
     return render_template('templates/stock-market-range.html' , range=ans , rangepopup=True)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('templates/error.html' , error="404 Page not found" ,solution="Avoid visiting this page, as it does not exist.")
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('templates/error.html' , error="500 Internal Server Error" , solution="You did something wrong and the server is not able to handle it or the server is down")
+
+@app.errorhandler(403)
+def forbidden(e):
+    return render_template('templates/error.html' , error="403 Forbidden" , solution="You are not allowed to access this page")
+
+@app.errorhandler(400)
+def bad_request(e):
+    return render_template('templates/error.html' , error="400 Bad Request" , solution="You did something wrong and the server is not able to handle it")
+
+@app.errorhandler(405)
+def method_not_allowed(e):
+    return render_template('templates/error.html' , error="405 Method Not Allowed" , solution="You did something wrong and the server is not able to handle it")
+
 
 if __name__ == '__main__':
     app.run(debug=True , host="0.0.0.0" , port=80)
--- projects/yt_video_downloader.py
+++ projects/yt_video_downloader.py
@@ -1,10 +1,7 @@
 from pytube import YouTube
 
 
-def get_video_download_url(url):
-    try:
-        youtube = YouTube(url)
-        video = youtube.streams.get_highest_resolution()
-        return video.url
-    except Exception as e:
-        return print(e)
+def get_video_download_url(url):    
+    youtube = YouTube(url)
+    video = youtube.streams.get_highest_resolution()
+    return video.url
